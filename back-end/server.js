855fb86c350be6a7292113cf6b8df301ea9f3ff4
katie-wright <ka.wright@hotmail.com>
2017-09-25 14:43:08 -0400

    back endpoints first draft

---
+++ back-end/server.js
@@ -0,0 +1,91 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, ()=>{
+    console.log("Listening on port", PORT);
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, authorization");
+  next();
+});
+
+const contacts = [];
+function Contact(firstName, lastName, phoneNumber, email) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.phoneNumber = phoneNumber;
+    this.email = email;
+    this.fullName = firstName + lastName;
+
+    return this;
+}
+
+app.get('/contacts', (req,res)=>{
+    res.json(contacts);
+});
+
+app.get('/contacts/:fullName', (req,res)=>{
+    let contact = contacts.find(c=>{
+        return c.fullName.toLowerCase() === req.params.fullName.toLowerCase()
+    })
+
+    res.json(contact);
+});
+
+app.post('/contacts', (req,res)=>{
+    let existingContact = contacts.find(c=>{
+        return c.fullName.toLowerCase() === req.body.firstName.toLowerCase()+req.body.lastName.toLowerCase()
+    });
+    if (!existingContact) {
+        let newContact = new Contact(req.body.firstName, req.body.lastName, req.body.phoneNumber, req.body.email);
+        contacts.push(newContact);
+
+        res.json(contacts);
+    }
+    else {
+        res.status(200);
+        res.send("Contact already exists");
+    }
+    
+});
+
+app.put('/contacts/:fullName', (req,res)=>{
+    let updatedContact = req.body;
+    let existingContact = contacts.find(c=>{
+        return c.fullName.toLowerCase() === req.body.firstName.toLowerCase()+req.body.lastName.toLowerCase()
+    });
+    if (!existingContact) {
+        for (let i=0; i<contacts.length; i++) {
+            if (contacts[i].fullName.toLowerCase()===req.params.fullName.toLowerCase()){
+                contacts[i]=updatedContact;
+                break;
+            }
+        }
+
+        res.json(contacts);
+    }
+    else {
+        res.status(200);
+        res.send("Contact already exists");
+    }
+});
+
+app.delete('/contacts/:fullName', (req,res)=>{
+    for (let i=0; i<contacts.length; i++) {
+            if (contacts[i].fullName.toLowerCase()===req.params.fullName.toLowerCase()){
+                contacts.splice(i,1);
+                break;
+            }
+        }
+
+    res.json(contacts);
+});
