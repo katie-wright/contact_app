e332285e1eae80dc1f08a2446aea6cd11d8562cf
katie-wright <ka.wright@hotmail.com>
2017-09-25 15:02:59 -0400

    fixed put route

--- back-end/server.js
+++ back-end/server.js
@@ -60,23 +60,26 @@ app.post('/contacts', (req,res)=>{
 
 app.put('/contacts/:fullName', (req,res)=>{
     let updatedContact = req.body;
-    let existingContact = contacts.find(c=>{
-        return c.fullName.toLowerCase() === req.body.firstName.toLowerCase()+req.body.lastName.toLowerCase()
-    });
-    if (!existingContact) {
-        for (let i=0; i<contacts.length; i++) {
-            if (contacts[i].fullName.toLowerCase()===req.params.fullName.toLowerCase()){
-                contacts[i]=updatedContact;
-                break;
-            }
+    let newFullName = updatedContact.firstName + updatedContact.lastName;
+    updatedContact.fullName = newFullName;
+    if (req.params.fullName.toLowerCase() !== newFullName.toLowerCase()) {
+        let existingContact = contacts.find(c=>{
+            return c.fullName.toLowerCase() === newFullName.toLowerCase()
+        });
+        if (existingContact) {
+            res.status(200);
+            res.send("Contact already exists");
+            return;
         }
+    }
+    for (let i=0; i<contacts.length; i++) {
+        if (contacts[i].fullName.toLowerCase()===req.params.fullName.toLowerCase()){
+            contacts[i]=updatedContact;
+            break;
+        }
+    }
 
-        res.json(contacts);
-    }
-    else {
-        res.status(200);
-        res.send("Contact already exists");
-    }
+    res.json(contacts);
 });
 
 app.delete('/contacts/:fullName', (req,res)=>{
